6acb64b30c79c22995e8888fbe7ec543d20bc831
Jaballah seif <88405083+jaballahseif@users.noreply.github.com>
2023-04-23 14:56:51 +0100

    update

--- src/app/student/document/document.component.ts
+++ src/app/student/document/document.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
-  selector: 'app-document',
+  selector: 'app-documents',
   templateUrl: './document.component.html',
   styleUrls: ['./document.component.css']
 })
-export class DocumentComponent {
+export class DocumentComponent implements OnInit {
+  files: string[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    
+    this.http.get<string[]>('http://localhost/php/view-document.php').subscribe({
+      next: (response) => {
+        this.files = response;
+      },
+      error: (error) => {
+        console.log('Error fetching files:', error);
+      }
+    });
+  }
+  downloadFile(file: string) {
+    window.open(`http://localhost/php/uploads/${file}`, '_blank');
+  }
 }
--- src/app/teacher/t-document/t-document.component.ts
+++ src/app/teacher/t-document/t-document.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-t-document',
@@ -7,10 +7,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./t-document.component.css']
 })
 export class TDocumentComponent {
+  title: string = '';
   selectedFile: File | null | undefined;
   uploadSuccess: boolean = false;
   uploadError: boolean = false;
-
 
   constructor(private http: HttpClient) { }
 
@@ -20,14 +20,33 @@ export class TDocumentComponent {
       this.selectedFile = file;
     }
   }
-  
 
   onSubmit() {
-    const formData = new FormData();
-    if(this.selectedFile){
-    formData.append('file', this.selectedFile, this.selectedFile.name);}
+    if (!this.title) {
+      console.log('Title is required');
+      return;
+    }
 
+    const formData = new FormData();
+    if (this.selectedFile) {
+      console.log(this.selectedFile);
+      console.log(this.title);
+      formData.append('file', this.selectedFile, this.selectedFile.name);
+    }
+    formData.append('title', this.title);
+
+    // Call the PHP file for inserting data into the database
     this.http.post<any>('http://localhost/php/add-file.php', formData).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    // Call the PHP file for uploading files
+    this.http.post<any>('http://localhost/php/upload-file.php', formData).subscribe(
       (response) => {
         console.log(response);
         this.uploadSuccess = true;
