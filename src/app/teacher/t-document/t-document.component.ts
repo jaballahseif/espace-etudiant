74d825699199d10c767be4584b6f5b433958cd5f
Jaballah seif <88405083+jaballahseif@users.noreply.github.com>
2023-04-23 01:07:04 +0100

    upload

--- src/app/teacher/t-document/t-document.component.ts
+++ src/app/teacher/t-document/t-document.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-t-document',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./t-document.component.css']
 })
 export class TDocumentComponent {
+  selectedFile: File | null | undefined;
+  uploadSuccess: boolean = false;
+  uploadError: boolean = false;
 
+
+  constructor(private http: HttpClient) { }
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+    }
+  }
+  
+
+  onSubmit() {
+    const formData = new FormData();
+    if(this.selectedFile){
+    formData.append('file', this.selectedFile, this.selectedFile.name);}
+
+    this.http.post<any>('http://localhost/php/add-file.php', formData).subscribe(
+      (response) => {
+        console.log(response);
+        this.uploadSuccess = true;
+        this.uploadError = false;
+      },
+      (error) => {
+        console.log(error);
+        this.uploadSuccess = false;
+        this.uploadError = true;
+      }
+    );
+  }
 }
